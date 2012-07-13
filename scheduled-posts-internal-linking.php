8204dc1ce4f87d4606ef9d7331c5396f3fbd9c4a
evansolomon <evansolomon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-13 07:01:32 +0000

    First pass at a plugin to add scheduled posts to the internal linking dialog in WordPress
    git-svn-id: http://plugins.svn.wordpress.org/internal-linking-for-scheduled-posts/trunk@571621 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scheduled-posts-internal-linking.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+Plugin Name: Internal Linking for Scheduled Posts
+Description: Add scheudled posts to the internal linking dialog in the WordPress editor.
+Version: 1.0
+Author: Evan Solomon
+Author URI: http://evansolomon.me
+*/
+
+/**
+ * Copyright (c) 2012 Evan Solomon.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
+
+function es_internal_linking_for_scheduled_posts( $query ) {
+	// Make sure this is a requent for internal linking
+	if ( ! isset( $_POST ) || ! isset( $_POST['action'] ) || 'wp-link-ajax' != $_POST['action'] )
+		return false;
+
+	// Add scheduled posts to the query
+	$post_status = (array) $query->query_vars['post_status'];
+	$post_status[] = 'future';
+	$query->set( 'post_status', $post_status );
+}
+add_action( 'pre_get_posts', 'es_internal_linking_for_scheduled_posts' );
